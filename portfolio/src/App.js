927c1e0ae68df08c014306dbe2ca28158a8bc159
mel <muhrich6@gmail.com>
2021-08-06 14:59:53 -0500

    resume link added

--- portfolio/src/App.js
+++ portfolio/src/App.js
@@ -3,7 +3,6 @@ import { Route, Switch, BrowserRouter as Router } from 'react-router-dom';
 import Header from './components/Header';
 import HomePage from './pages/HomePage';
 import ContactPage from './pages/ContactPage';
-// import ResumePage from './pages/ResumePage';
 
 function App() {
   return (
@@ -11,9 +10,8 @@ function App() {
       <Router>
         <Header />
         <Switch>
-          <Route exact path="/" component={HomePage} />
-          <Route exact path="/contact" component={ContactPage} /> 
-          {/* <Route exact path="/resume" component={ResumePage} />  */}
+          <Route exact path='/' component={HomePage} />
+          <Route exact path='/contact' component={ContactPage} /> 
         </Switch>
       </Router>
      
--- portfolio/src/components/AboutMe/index.js
+++ portfolio/src/components/AboutMe/index.js
@@ -5,15 +5,8 @@ function AboutMe() {
     return (
         <div>
             <h4 className='about-me' id='about-me'>About Me</h4>
-            <p className='bio'> I am a recent graduate from the University of Illinois Urbana-Champaign living in the Chicago area.
-            After earning my degree in International Relations as well as a minor Spanish, I decided that I
-            wanted to further enhance my skills, leading to the decision to become a web developer. I wanted to
-            pursue a challenging, creative career path that would give me plenty of options, opportunities, and room
-            to grow.
-            </p>
-            <p className='bio2'>When I'm not working or coding, I enjoy making music, walking my dogs, and roasting coffee! Feel free to
-            check out some of my work below.
-            </p>
+            <p className='bio'> University of Illinois graduate & Web Developer from Chicago, IL.</p>
+            <p className='bio2'>Please check out some of my work below and feel free to contact me!</p>
         </div>
     )
 };
