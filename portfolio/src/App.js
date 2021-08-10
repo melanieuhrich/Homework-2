0f327ad9812317206e4ba0bf6cd924939d39cca6
mel <muhrich6@gmail.com>
2021-08-10 17:28:23 -0500

    resume

--- portfolio/src/App.js
+++ portfolio/src/App.js
@@ -3,7 +3,7 @@ import { Route, Switch, BrowserRouter as Router } from 'react-router-dom';
 import Home from './pages/Home';
 import AboutMe from './pages/AboutMe';
 import Work from './pages/Work';
-import Resume from './pages/Resume';
+// import Resume from './pages/Resume';
 import Navbar from './components/Navbar';
 
 function App() {
@@ -17,7 +17,7 @@ function App() {
             <Route exact path='/' component={Home} />
               <Route exact path='/about' component={AboutMe} />
               <Route exact path='/work' component={Work} />
-              <Route exact path='/resume' component={Resume} />
+              {/* <Route exact path='/resume' component={Resume} /> */}
             </Switch>
           </Router>
         </header>
--- portfolio/src/components/Navbar/index.js
+++ portfolio/src/components/Navbar/index.js
@@ -40,14 +40,16 @@ function Navbar() {
             </Link></NavText>
                 </NavItem>
                 <NavItem eventKey="resume">
-                    <NavIcon><Link to='/resume'>
+                    <NavIcon>
+                        <a href='https://docs.google.com/document/d/1MVuJxW494eAKX1W6SvwsmSc9zPWj18lXjd0-1KWzzYE/edit?usp=sharing' target='_blank'>
                         <svg xmlns="http://www.w3.org/2000/svg" width="16" height="16" fill="currentColor" class="bi bi-briefcase" viewBox="0 0 16 16" style={{ color: 'rgb(229,199,208)' }}>
                             <path d="M6.5 1A1.5 1.5 0 0 0 5 2.5V3H1.5A1.5 1.5 0 0 0 0 4.5v8A1.5 1.5 0 0 0 1.5 14h13a1.5 1.5 0 0 0 1.5-1.5v-8A1.5 1.5 0 0 0 14.5 3H11v-.5A1.5 1.5 0 0 0 9.5 1h-3zm0 1h3a.5.5 0 0 1 .5.5V3H6v-.5a.5.5 0 0 1 .5-.5zm1.886 6.914L15 7.151V12.5a.5.5 0 0 1-.5.5h-13a.5.5 0 0 1-.5-.5V7.15l6.614 1.764a1.5 1.5 0 0 0 .772 0zM1.5 4h13a.5.5 0 0 1 .5.5v1.616L8.129 7.948a.5.5 0 0 1-.258 0L1 6.116V4.5a.5.5 0 0 1 .5-.5z" />
                         </svg>
-                    </Link></NavIcon>
-                    <NavText><Link to='/resume' style={{ color: 'rgb(229,199,208)', textDecoration: 'none' }}>
+                    </a>
+                    </NavIcon>
+                    <NavText><a href='https://docs.google.com/document/d/1MVuJxW494eAKX1W6SvwsmSc9zPWj18lXjd0-1KWzzYE/edit?usp=sharing' target='_blank' style={{ color: 'rgb(229,199,208)', textDecoration: 'none' }}>
                         Resumé
-            </Link></NavText>
+            </a></NavText>
                 </NavItem>
             </SideNav.Nav>
         </SideNav>
