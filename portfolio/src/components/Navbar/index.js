58b5550e0ce356d19ec541689c26fe91a0db89c8
mel <muhrich6@gmail.com>
2021-09-16 20:48:28 -0500

    updates

--- portfolio/src/components/Navbar/index.js
+++ portfolio/src/components/Navbar/index.js
@@ -1,6 +1,6 @@
 import React from 'react';
 import { Link } from 'react-router-dom';
-import SideNav, { Toggle, Nav, NavItem, NavIcon, NavText } from '@trendmicro/react-sidenav';
+import SideNav, { NavItem, NavIcon, NavText } from '@trendmicro/react-sidenav';
 import '@trendmicro/react-sidenav/dist/react-sidenav.css';
 
 function Navbar() {
@@ -41,13 +41,13 @@ function Navbar() {
                 </NavItem>
                 <NavItem eventKey="resume">
                     <NavIcon>
-                        <a href='https://docs.google.com/document/d/1MVuJxW494eAKX1W6SvwsmSc9zPWj18lXjd0-1KWzzYE/edit?usp=sharing' target='_blank'>
+                        <a href='https://docs.google.com/document/d/1MVuJxW494eAKX1W6SvwsmSc9zPWj18lXjd0-1KWzzYE/edit?usp=sharing' target='_blank' rel="noreferrer">
                         <svg xmlns="http://www.w3.org/2000/svg" width="16" height="16" fill="currentColor" class="bi bi-briefcase" viewBox="0 0 16 16" style={{ color: 'rgb(229,199,208)' }}>
                             <path d="M6.5 1A1.5 1.5 0 0 0 5 2.5V3H1.5A1.5 1.5 0 0 0 0 4.5v8A1.5 1.5 0 0 0 1.5 14h13a1.5 1.5 0 0 0 1.5-1.5v-8A1.5 1.5 0 0 0 14.5 3H11v-.5A1.5 1.5 0 0 0 9.5 1h-3zm0 1h3a.5.5 0 0 1 .5.5V3H6v-.5a.5.5 0 0 1 .5-.5zm1.886 6.914L15 7.151V12.5a.5.5 0 0 1-.5.5h-13a.5.5 0 0 1-.5-.5V7.15l6.614 1.764a1.5 1.5 0 0 0 .772 0zM1.5 4h13a.5.5 0 0 1 .5.5v1.616L8.129 7.948a.5.5 0 0 1-.258 0L1 6.116V4.5a.5.5 0 0 1 .5-.5z" />
                         </svg>
                     </a>
                     </NavIcon>
-                    <NavText><a href='https://docs.google.com/document/d/1MVuJxW494eAKX1W6SvwsmSc9zPWj18lXjd0-1KWzzYE/edit?usp=sharing' target='_blank' style={{ color: 'rgb(229,199,208)', textDecoration: 'none' }}>
+                    <NavText><a href='https://docs.google.com/document/d/1MVuJxW494eAKX1W6SvwsmSc9zPWj18lXjd0-1KWzzYE/edit?usp=sharing' target='_blank' rel="noreferrer" style={{ color: 'rgb(229,199,208)', textDecoration: 'none' }}>
                         Resumé
             </a></NavText>
                 </NavItem>
